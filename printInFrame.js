f21eb8345faebc18986a752dd6e2bbf940fccaa2
Jonas-Kunz <jonas_K_7@outlook.com>
2024-05-21 16:22:54 -0300

    Fix print inFrame

--- printInFrame.js
+++ printInFrame.js
@@ -1,16 +1,16 @@
-const printInFrame = function(list) {
-  list = list.split(' ');
+const printInFrame = function (list) {
+  list = list.split(" ");
   const longest = longestStr(list).length;
-  const border = repeat('*', longest);
+  const border = repeat("*", longest + 3);
 
   console.log(border);
   for (const word of list) {
-    console.log(`* ${word}${repeat(' ', longest - word.length + 1)}*`);
+    console.log(`* ${word}${repeat(" ", longest - word.length)}*`);
   }
   console.log(border);
 };
 
-const repeat = function(str, times) {
+const repeat = function (str, times) {
   let result = str;
 
   for (let i = 0; i < times; i++) {
@@ -20,18 +20,20 @@ const repeat = function(str, times) {
   return result;
 };
 
-const longestStr = function(list) {
+const longestStr = function (list) {
   let longest = list[0];
 
   for (const str of list) {
-    longest = str;
+    if (str.length > longest.length) {
+      longest = str;
+    }
   }
 
   return longest;
 };
 
 // Test driver code, do not modify
-printInFrame('May the force be with you');
+printInFrame("May the force be with you");
 printInFrame('Here\'s Johnny!');
 printInFrame('Supercalifragalisticexpialadocious');
 printInFrame('Lost, like tears in the rain');
